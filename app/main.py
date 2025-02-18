6ebac73334227561477a32e40dcabb19a22b6a86
git_practic <dogoberrr@gmail.com>
2025-02-18 20:14:57 +0200

    fix flake

--- app/main.py
+++ app/main.py
@@ -9,16 +9,11 @@ URL = "https://api.weatherapi.com/v1/current.json?"
 FILTERING = "Paris"
 API_KEY = os.getenv("API_KEY")
 
+
 def get_weather() -> None:
     print("Performing request to Weather API for city Paris...")
 
-    result = requests.get(
-        URL,
-        params={
-            "key": API_KEY,
-            "q": FILTERING
-        }
-    ).json()
+    result = requests.get(URL, params={"key": API_KEY, "q": FILTERING}).json()
 
     city = result["location"]["name"]
     country = result["location"]["country"]
@@ -26,7 +21,9 @@ def get_weather() -> None:
     temp_c = result["current"]["temp_c"]
     condition = result["current"]["condition"]["text"]
 
-    print(f"{city}/{country} {local_time} Weather: {temp_c} Celsius, {condition}")
+    print(
+        f"{city}/{country} {local_time} Weather: {temp_c} Celsius, {condition}"
+    )
     pass
 
 
